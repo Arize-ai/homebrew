4e26d0c549b9ca5ee2ea6f049d45b9fd69905cc7
michaelschiff <schiff.michael@gmail.com>
2023-01-04 09:38:26 -0800

    fix URL;

--- Formula/antlr.rb
+++ Formula/antlr.rb
@@ -1,7 +1,7 @@
 class Antlr < Formula
   desc ""
   homepage "github.com/michaelschiff/taps"
-  url "https://github.com/michaelschiff/taps/releases/download/v2/antlr-4.11-complete.jar"
+  url "https://github.com/michaelschiff/taps/releases/download/v2/antlr-4.11.1-complete.jar"
   version "v1"
   sha256 ""
   license ""
