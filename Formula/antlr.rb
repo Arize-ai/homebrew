34ade9b895e0c293342409d064efc6e818d4503d
michaelschiff <schiff.michael@gmail.com>
2023-01-04 09:31:09 -0800

    antlr

---
+++ Formula/antlr.rb
@@ -0,0 +1,15 @@
+class ANTLR < Formula
+  desc ""
+  homepage "github.com/michaelschiff/taps"
+  url "https://github.com/michaelschiff/taps/releases/download/v2/antlr-4.11-complete.jar"
+  version "v1"
+  sha256 ""
+  license ""
+
+  def install
+    libexec.install Dir["*"]
+
+    bin.write_jar_script libexec/"antlr-4.11-complete.jar", "antlr"
+  end
+
+end
