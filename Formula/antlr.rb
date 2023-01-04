78b8da661d4ce5cca60f1006d4e8357b9dea906d
michaelschiff <schiff.michael@gmail.com>
2023-01-04 09:39:31 -0800

    sha

--- Formula/antlr.rb
+++ Formula/antlr.rb
@@ -3,7 +3,7 @@ class Antlr < Formula
   homepage "github.com/michaelschiff/taps"
   url "https://github.com/michaelschiff/taps/releases/download/v2/antlr-4.11.1-complete.jar"
   version "v1"
-  sha256 ""
+  sha256 "62975e192b4af2622b72b5f0131553ee3cbce97f76dc2a41632dcc55e25473e1"
   license ""
 
   def install
